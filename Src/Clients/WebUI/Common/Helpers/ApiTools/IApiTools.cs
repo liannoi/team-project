36eed4ce670f2312211f8b89891cc87e3e20cdc3
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-04-08 16:25:59 +0300

    Prepared the foundation for the implementation of the MVC client

---
+++ Src/Clients/WebUI/Common/Helpers/ApiTools/IApiTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TeamProject.Clients.WebUI.Common.Helpers.ApiTools
+{
+    public interface IApiTools
+    {
+        Task<TEntity> FetchAsync<TEntity>(string uri) where TEntity : class, new();
+        Task<TEntity> PostAsync<TEntity>(string uri, ByteArrayContent content) where TEntity : class, new();
+        Task<TEntity> PostAsync<TEntity>(string uri, object value) where TEntity : class, new();
+        Task DeleteAsync(string uri);
+    }
+}
