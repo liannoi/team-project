3a4967a3f03fc068012f48e5ae3a3703fac3ba23
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-04-13 15:32:45 +0300

    Implemented the logic of handling "emergency situations"

--- Src/Clients/WebUI/Controllers/AccountController.cs
+++ Src/Clients/WebUI/Controllers/AccountController.cs
@@ -13,13 +13,13 @@ using TeamProject.Domain.Entities;
 
 namespace TeamProject.Clients.WebUI.Controllers
 {
-    public class AccountsController : BaseController
+    public class AccountController : BaseController
     {
         private readonly IApiTools _apiTools;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountsController(IApiTools apiTools, SignInManager<AppUser> signInManager,
+        public AccountController(IApiTools apiTools, SignInManager<AppUser> signInManager,
             RoleManager<IdentityRole> roleManager)
         {
             _apiTools = apiTools;
@@ -61,15 +61,28 @@ namespace TeamProject.Clients.WebUI.Controllers
             #endregion
         }
 
+        /// <summary>
+        ///     Sign in.
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult SignIn()
+        public async Task<IActionResult> Login(string returnUrl = null)
         {
+            await SignOutAsync();
+            ViewData["ReturnUrl"] = returnUrl;
+
             return View();
         }
 
+        /// <summary>
+        ///     Sign in.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> SignIn([FromForm] LoginViewModel model)
+        public async Task<IActionResult> Login([FromForm] LoginViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
 
@@ -84,7 +97,7 @@ namespace TeamProject.Clients.WebUI.Controllers
                 HttpContext.Response.Cookies.Append(MvcClientDefaults.InCookiesJwtTokenName, result.Token);
                 await _signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
 
-                return RedirectToAction("Index", "Home");
+                return RedirectToLocal(ViewData["ReturnUrl"] as string);
             }
 
             AddErrors(result.Errors);
@@ -97,15 +110,37 @@ namespace TeamProject.Clients.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> SignOut()
         {
-            HttpContext.Response.Cookies.Delete(MvcClientDefaults.InCookiesJwtTokenName);
-            await _signInManager.SignOutAsync();
+            await SignOutAsync();
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        #region Helpers
 
         private IEnumerable<SelectListItem> Roles()
         {
             return _roleManager.Roles.Select(x => new SelectListItem(x.Name, x.Id.ToString()));
         }
+
+        private async Task SignOutAsync()
+        {
+            HttpContext.Response.Cookies.Delete(MvcClientDefaults.InCookiesJwtTokenName);
+            await _signInManager.SignOutAsync();
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            return string.IsNullOrEmpty(returnUrl)
+                ? (IActionResult) RedirectToAction("Index", "Home")
+                : Redirect(returnUrl);
+        }
+
+        #endregion
     }
 }
