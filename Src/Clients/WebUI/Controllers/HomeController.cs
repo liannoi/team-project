97f2c46e7333e568e7c1c1985935af1563d62e14
Yarosalv <aim4rip@gmail.com>
2020-04-13 10:23:29 +0300

    Added functional

--- Src/Clients/WebUI/Controllers/HomeController.cs
+++ Src/Clients/WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TeamProject.Clients.Common.Tools;
@@ -22,8 +23,19 @@ namespace TeamProject.Clients.WebUI.Controllers
 
         public async Task<IActionResult> Actors()
         {
-            var model = await _apiTools.FetchAsync<List<ActorReturnModel>>("https://localhost:5001/api/actors/getall");
+            var model = await _apiTools.FetchAsync<List<ActorReturnModel>>("https://localhost:44378/api/actors/getall");
             return View(model);
+        }
+       
+        public ActionResult AddActor()
+        {            
+            return View(new ActorReturnModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddActor(ActorReturnModel _actor)
+        {
+            await _apiTools.PostAsync<ActorReturnModel>("https://localhost:44378/api/actors/add", _actor);
+            return RedirectToAction("Actors");
         }
     }
 }
---
+++ Src/Core/Application/Storage/Films/FilmRepository.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamProject.Application.Common.Interfaces;
+using TeamProject.Domain.Entities;
+
+namespace TeamProject.Application.Storage.Films
+{
+    class FilmRepository:BaseBusinessService<Film,FilmDTO>
+    {
+        public FilmRepository(IDataService<Film> dataService, IMapper mapper):base(dataService,mapper)
+        {
+        }
+    }
+}
