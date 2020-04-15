94bb6c436baafecf75d65f269302676a4037dbac
Yarosalv <aim4rip@gmail.com>
2020-04-15 22:40:56 +0300

    WebUi GenreController

---
+++ Src/Clients/Core/Common/Models/Storage/Genres/GenreBindingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject.Clients.Common.Models.Storage.Genres
+{
+    public class GenreBindingModel
+    {
+        public int GenreId { get; set; }
+        public string Title { get; set; }
+    }
+}
